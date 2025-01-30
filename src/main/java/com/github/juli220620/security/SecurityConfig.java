1a9b504fb2ac6f5535c5dc2fc81223be10642567
Julia R <juli220620@gmail.com>
2025-01-30 20:51:31 +0300

    Generally configured security for jwt-authentication. Created JwtService for generating tokens.

---
+++ src/main/java/com/github/juli220620/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.github.juli220620.security;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.JWK;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Objects;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Value("${app.security.pub.key.path}")
+    private String publicKeyPath;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(
+                reg -> reg
+                        .requestMatchers("/api/book/**").authenticated()
+                        .anyRequest().permitAll())
+                .sessionManagement(it -> it.sessionCreationPolicy(SessionCreationPolicy.NEVER))
+                .logout(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(it -> it.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withPublicKey(publicKey()).build();
+    }
+
+    private RSAPublicKey publicKey() {
+        try(var input = getClass().getClassLoader().getResourceAsStream(publicKeyPath)) {
+            return JWK.parseFromPEMEncodedObjects(
+                    new String(Objects.requireNonNull(input).readAllBytes())
+                    )
+                    .toRSAKey()
+                    .toRSAPublicKey();
+        } catch (IOException | JOSEException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
