63a4b666afd82f0c01b7bac90ffd023d66d4c9e5
Julia R <juli220620@gmail.com>
2025-03-27 21:25:51 +0300

    Added image microservice.

---
+++ image/src/main/java/com/github/juli220620/controller/BookServiceController.java
@@ -0,0 +1,25 @@
+package com.github.juli220620.controller;
+
+import com.github.juli220620.model.BookDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Optional;
+
+@FeignClient(value = "book-service")
+public interface BookServiceController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/book/{id}")
+    Optional<BookDto> getBookById(@PathVariable Long id);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/image/{bookId}")
+    Optional<String> getImageId(@PathVariable Long bookId);
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/save")
+    void saveBook(BookDto bookDto);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/book/exists/{bookId}")
+    boolean entryExists(@PathVariable Long bookId);
+}
---
+++ image/src/main/java/com/github/juli220620/model/BookDto.java
@@ -0,0 +1,14 @@
+package com.github.juli220620.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDto {
+
+    private Long id;
+    private String imageId;
+}
